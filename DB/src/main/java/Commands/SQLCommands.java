72bb5bcacbd602323f5bf0af38ecb30c363a0b78
Galfest <abramyants4@gmail.com>
2023-01-19 14:05:47 +0300

    v1

---
+++ DB/src/main/java/Commands/SQLCommands.java
@@ -0,0 +1,12 @@
+package Commands;
+
+import com.mysql.cj.jdbc.ConnectionImpl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLCommands {
+
+}
+
--- DB/src/main/java/System/JDBC.java
+++ DB/src/main/java/System/JDBC.java
@@ -1,2 +1,14 @@
-package System;public class jdbc {
+package System;
+
+import static System.DBDespather.connectToDB;
+
+public class JDBC {
+
+    // JDBC driver name and database URL
+//    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+
+    public static void main(String[] args) {
+        connectToDB();
+    }
 }
